1e6dd355fc8d96c541c7e23e565af318f7e3e556
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-12-12 16:37:50 +0100

    Initial a72q tree
    Change-Id: I547988523152a4a0d2514658a874c2cfc98e131c

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/a72q
+
+include device/samsung/sm7125-common/BoardConfigCommon.mk
+
+# Kernel
+TARGET_KERNEL_CONFIG        := vendor/lineage-a72q_defconfig
+BOARD_NAME                  := SRPTJ06B001
+
+# Display
+TARGET_SCREEN_DENSITY := 450
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := a72q
