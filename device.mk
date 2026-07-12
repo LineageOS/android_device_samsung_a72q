359896604fdb0f80e8029eb3b671f1b87e1b230c
Simon1511 <me@simon1511.de>
2026-07-12 17:09:24 +0200

    a72q: re-enable the macro camera lens
    a72q has both telephoto and macro lenses. This was missed when we switched from HIDL -> AIDL
    
    Change-Id: I63d6ba8bb4dc86f376c01411fa7dbda80ac65f37

--- device.mk
+++ device.mk
@@ -31,4 +31,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Camera
-$(call soong_config_set,samsungCameraVars,extra_ids,52) # ID=52 is telephoto
+$(call soong_config_set,samsungCameraVars,extra_ids,52,54) # ID=52 is telephoto, ID=54 is macro
